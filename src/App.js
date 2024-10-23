29521ed17cbbd35cb82d45f21171424f371de4b8
Brandon Gonzalez <brajgon@gmail.com>
2024-10-23 12:19:44 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Alert,
+  Breadcrumb,
+  Card,
+  Form,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <Form>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="password" />
+            </Form.Group>
+            <Button variant="secondary" type="submit">
+              Login
+            </Button>
+          </Form>
+
+          <Card className="mb-3" style={{ color: "#000" }}>
+            <Card.Img src="https://picsum.photos/200/100" />
+            <Card.Body>
+              <Card.Title>Card Example</Card.Title>
+              <Card.Text>This is an example</Card.Text>
+              <Button variant="primary">Read More</Button>
+            </Card.Body>
+          </Card>
+          <Breadcrumb>
+            <Breadcrumb.Item>Test</Breadcrumb.Item>
+            <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+            <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+          </Breadcrumb>
+          <Alert variant="success">This is a button</Alert>
+          <Button>Test</Button>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
